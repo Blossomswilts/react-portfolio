3d65fe3245e0d1c550d5c8a7b5c8566140266f25
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-08-26 13:26:13 -0400

    added footer construct

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Outlet } from "react-router-dom";
 import Navigation from "./components/Nav";
 
 function App() {
-    return <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-        <Navigation />
-        <main className="mx-3">
-            <Outlet />
-        </main>
-        <Footer />
-    </div>;
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <Navigation />
+            <main className="mx-3">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
+import Card from 'react-bootstrap/Card';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <div>
-            <h1>Footer</h1>
+            <Card className="text-center" bg="dark" data-bs-theme="dark" expand='lg'>
+      <Card.Body>
+        <Card.Title>&quot;Don&apos;t judge each day by the harvest you reap but by the seeds that you plant.&quot;
+
+- Robert Louis Stevenson</Card.Title>
+        <Card.Text>
+          Check out my LinkedIn and GitHub!
+        </Card.Text>
+        <Link to="https://www.linkedin.com/in/michael-tranquillo/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></Link>
+        <Link to="https://github.com/Blossomswilts" target="_blank" rel="noopener noreferrer"><FaGithub /></Link>
+      </Card.Body>
+      <Card.Footer className="text-muted">Michael Tranquillo</Card.Footer>
+    </Card>
         </div>
     )
 }
