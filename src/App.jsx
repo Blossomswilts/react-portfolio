7f600626bf6f070b2ee4f3ddb82ff197f67a349d
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-08-28 21:36:46 -0400

    fixed style css import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer";
 import Navigation from "./components/Nav";
 import { Outlet } from "react-router-dom";
 import ParticleBackground from "./components/PatricleBackground";
-import "../src/styles/App.css";
+import "../src/styles/app.css";
 
 function App() {
     return (
