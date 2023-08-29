25de9d53427d7334826980cc09c8a89a4fd2bae2
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-08-28 22:12:59 -0400

    file name change from nav to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer";
-import Navigation from "./components/Nav";
+import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import ParticleBackground from "./components/PatricleBackground";
 import "../src/styles/app.css";
@@ -9,7 +9,7 @@ function App() {
         <>
             <ParticleBackground />
             <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-                <Navigation />
+                <Header />
                 <main className="mx-3" >
                     <Outlet />
                 </main>
