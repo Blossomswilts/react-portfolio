30bcff489ef45ca5a5b687acdaee8c57c8d7c4fc
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-08-28 22:02:22 -0400

    added photos to public dir

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,7 @@
 const PortfolioItems = [
     {
         title: "Donomon Choice RPG",
-        imgUrl: "../src/assets/images/donomonlogo.png",
+        imgUrl: "/assets/images/donomonlogo.png",
         description:
             "Within this application, you are able to decide the evolution of your character based on the choices you make. ",
         tech: "MVC, MySQL, Sequelize, Express, Handlebars, Node.js, JavaScript, HTML, CSS, Bootstrap, Heroku",
@@ -9,7 +9,7 @@ const PortfolioItems = [
     },
     {
         title: "Budget Planner",
-        imgUrl: "../src/assets/images/budgetplanner.png",
+        imgUrl: "/assets/images/budgetplanner.png",
         description:
             "This application allows you to track your budget by adding and subtracting funds, whilst being able to see your budget in a graph.",
         tech: "JavaScript, HTML, CSS, API, GraphJS",
