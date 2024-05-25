708c00d4ad6df9fed6b7d29a715001b3313725f3
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2024-05-25 11:24:28 -0400

    removed quote from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,67 +1,50 @@
-import Card from "react-bootstrap/Card";
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const style = {
-    position: "fixed",
-    bottom: "0",
-    right: "0",
-    left: "0",
+    position: 'fixed',
+    bottom: '0',
+    right: '0',
+    left: '0',
 };
 
 const linkStyles = {
-    color: "green",
-    fontSize: "2.5em",
-    transition: "color .15s ease-in-out",
-    margin: ".5em",
+    color: 'green',
+    fontSize: '2.5em',
+    transition: 'color .15s ease-in-out',
+    margin: '.5em',
 };
 
 function Footer() {
     return (
         <div style={style}>
-            <Card
-                className="text-center"
-                bg="dark"
-                data-bs-theme="dark"
-                expand="lg"
-            >
+            <Card className="text-center" bg="dark" data-bs-theme="dark" expand="lg">
                 <Card.Body>
-                    <Card.Title>
-                        &quot;Don&apos;t judge each day by the harvest you reap
-                        but by the seeds that you plant.&quot; - Robert Louis
-                        Stevenson
-                    </Card.Title>
-                    <Card.Text>Check out my Social Media!</Card.Text>
+                    <Card.Title>Check out my Social Media!</Card.Title>
                     <Link
                         to="https://www.linkedin.com/in/michael-tranquillo/"
                         target="_blank"
                         rel="noopener noreferrer"
-                        style={linkStyles}
-                    >
+                        style={linkStyles}>
                         <FaLinkedin />
                     </Link>
                     <Link
                         to="https://github.com/Blossomswilts"
                         target="_blank"
                         rel="noopener noreferrer"
-                        style={linkStyles}
-                    >
+                        style={linkStyles}>
                         <FaGithub />
                     </Link>
                     <Link
                         to="https://www.instagram.com/blossomswilt/"
                         target="_blank"
                         rel="noopener noreferrer"
-                        style={linkStyles}
-                    >
+                        style={linkStyles}>
                         <FaInstagram />
                     </Link>
                 </Card.Body>
-                <Card.Footer className="text-muted">
-                    Michael Tranquillo
-                </Card.Footer>
+                <Card.Footer className="text-muted">Michael Tranquillo</Card.Footer>
             </Card>
         </div>
     );
