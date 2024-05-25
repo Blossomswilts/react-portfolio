c4432c72c30a64d7b123db3d14b74d50c5301c3d
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2024-05-25 11:22:24 -0400

    removed particle background

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
-import ParticleBackground from "./components/PatricleBackground";
-import "../src/styles/app.css";
+import { Outlet } from 'react-router-dom';
+import '../src/styles/app.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
     return (
         <>
-            <ParticleBackground />
             <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
                 <Header />
-                <main className="mx-3" >
+                <main className="mx-3">
                     <Outlet />
                 </main>
                 <Footer />
